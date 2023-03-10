3d9b62cd3ea60fbc76214c75767f180a05907794
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2023-03-09 18:28:41 -0600

    Add files via upload

---
+++ Act-repo 1/Main.java
@@ -0,0 +1,12 @@
+///Estos son llamados clases
+public class Main {
+
+    ///Estos son llamados los metodos
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //El "System" lo vemos como una clase, el "out" como un objeto y el "print" como un metodo.
+        //Uno es con salto de linea y otro sin salto de linea
+        System.out.print("Hello world!");
+    }
+}
