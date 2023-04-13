301d6b08ebeecaf520d27361301106147bdb0976
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2023-04-13 06:50:32 -0600

    Add files via upload

---
+++ Act-repo 17-/Act-1/Testing.java
@@ -0,0 +1,26 @@
+import java.util.SortedSet;
+
+public class Testing {
+
+    // Enumeraciones
+
+    public static void main(String[] args)
+    {
+        /*
+        Figura fig = Figura.CORAZON;
+        System.out.println(fig);
+        fig = Figura.ESPADA;
+        System.out.println(fig);
+
+
+        Carta c1 = new Carta(Figura.CORAZON,CartaNum.AS);
+
+        System.out.println(c1);
+         */
+
+        Maso m1 = new Maso();
+        m1.crearMaso();
+
+        System.out.println(m1);
+    }
+}
