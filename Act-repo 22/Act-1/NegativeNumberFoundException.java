8681496098188116214c866fd2815a60393924e4
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2023-04-24 06:51:35 -0600

    Add files via upload

---
+++ Act-repo 22/Act-1/NegativeNumberFoundException.java
@@ -0,0 +1,18 @@
+
+@SuppressWarnings("serial")
+public class NegativeNumberFoundException extends Exception
+{
+    private double negativeNumber;
+
+    public NegativeNumberFoundException(double n)
+    {
+        super("NegativeNumberFoundException");
+        negativeNumber = n;
+    }
+
+
+    public String toString()
+    {
+        return getMessage() + "\nNegative number found: " + negativeNumber;
+    }
+}
