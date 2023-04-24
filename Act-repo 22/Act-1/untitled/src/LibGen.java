021ab5ef14a5efb5e85cc110331d77dd6530d2ae
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2023-04-24 08:36:23 -0600

    Final iter

--- Act-repo 22/Act-1/untitled/src/LibGen.java
+++ Act-repo 22/Act-1/untitled/src/LibGen.java
@@ -1,8 +1,10 @@
+import java.util.Collection;
 import java.util.List;
 
 public class LibGen
 {
 
+    // Caso 1
     public static void print(List<?> list)
     {
         for(int i=0;i<list.size();i++)
@@ -12,6 +14,57 @@ public class LibGen
         }
     }
 
+
+    // Caso 2
+    public static <T> int frequency(Collection<T> collection, T object)
+    {
+        int sent=0;
+        for (Object o:collection)
+        {
+            if(object.equals(o));
+            {
+                sent++;
+            }
+        }
+        return sent;
+    }
+
+    // Caso 3
+    public static int greatestInteger(Collection<Integer> collection)
+    {
+        Integer G = null;
+        for(Integer i: collection)
+        {
+            if(G == null || i > G)
+            {
+                G = i;
+            }
+        }
+
+        return G;
+    }
+
+    // Caso 4
+    public static double greatestNumber(Collection<Number> collection)
+    {
+        Number G = null;
+        for (Number T: collection)
+        {
+            if(G == null || T.doubleValue() > G.doubleValue())
+            {
+                G = T;
+            }
+        }
+
+        return G.doubleValue();
+    }
+
+
+    // Caso 5
+    public static double greatestNumber(Collection<? extends Number> collection)
+    {
+
+    }
 
 
 }
--- Act-repo 22/Act-1/untitled/src/TestLib.java
+++ Act-repo 22/Act-1/untitled/src/TestLib.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class TestLib {
     public static void main(String[] args)
     {
+
         Vector<String> strVector = new Vector<>();
 
         strVector.add("One");
@@ -10,12 +11,12 @@ public class TestLib {
         strVector.add("Three");
         strVector.add("Four");
         strVector.add("Five");
-        strVector.add("Six");
+        strVector.add("One");
         strVector.add("Seven");
         strVector.add("Eight");
-        strVector.add("Nine");
+        strVector.add("One");
         strVector.add("Ten");
-
+         /*
         LibGen.print(strVector);
 
         System.out.println("-----------------------------------------------------");
@@ -38,6 +39,70 @@ public class TestLib {
         System.out.println("-----------------------------------------------------");
 
         Stack<DateD> dtStack = new Stack<>();
+
+        dtStack.add(new DateD());
+        dtStack.add(new DateD(24,4,2023));
+        dtStack.add(new DateD());
+        dtStack.add(new DateD(1,1,2023));
+        dtStack.add(new DateD(1,5,2023));
+
+        LibGen.print(dtStack);
+
+        System.out.println("-----------------------------------------------------");
+
+        HashSet<String> strHashSet = new HashSet<>();
+
+        strHashSet.add("Uno");
+        strHashSet.add("Dos");
+        strHashSet.add("Tres");
+        strHashSet.add("Cuatro");
+        strHashSet.add("Cinco");
+
+        //LibGen.print(strHashSet);
+
+        //------------------------------------------------------------------------
+        HashSet<Integer> intHashSet = new HashSet<>();
+
+
+        intHashSet.add(1500);
+        intHashSet.add(2000);
+        intHashSet.add(3000);
+
+        LibGen.frequency(strVector,"Nine");
+        LibGen.frequency(intHashSet,1500);
+
+        System.out.println( LibGen.frequency(strVector,"One"));
+        System.out.println( LibGen.frequency(intHashSet,3000));
+        */
+
+        //------------------------------------------------------------------------
+        HashSet<Integer> intHashSet = new HashSet<>();
+
+
+        intHashSet.add(1500);
+        intHashSet.add(2000);
+        intHashSet.add(3000);
+        intHashSet.add(900);
+        intHashSet.add(200);
+        intHashSet.add(50);
+        intHashSet.add(42320);
+        intHashSet.add(12);
+
+        System.out.println( LibGen.frequency(strVector,"One"));
+        System.out.println( LibGen.frequency(intHashSet,1500));
+
+        System.out.println(LibGen.greatestInteger(intHashSet));
+
+
+        HashSet<Number> nmbSet = new HashSet<>();
+
+        nmbSet.add(3.5);
+        nmbSet.add(20);
+        nmbSet.add(1_000_000_000);
+        nmbSet.add(20000.345);
+
+        System.out.println("El numero mas grande en general es: " + LibGen.greatestNumber(nmbSet));
+
 
     }
 }
